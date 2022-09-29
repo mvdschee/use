26f02e32a6ca7b73eb9a63ab0c3f6366e6927dc0
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-09-29 22:33:28 +0800

    added avatar icon function

---
+++ lib/methodes/useAvatar.ts
@@ -0,0 +1,13 @@
+import { useToColor } from './useColor';
+
+export const useAvatar = (account: string): string => {
+    return `<svg x="0" y="0" viewbox="0 0 32 32" style="overflow:hidden;border-radius:50%;background-color:${useToColor(
+        account,
+        0.7
+    )}">
+<rect x="0" y="0" width="32" height="32" transform="translate(-8 0)" fill="${useToColor(account, 0.85)}"></rect>
+<rect x="0" y="0" width="32" height="32" transform="translate(-20 10)" fill="${useToColor(account, 0.7)}"></rect>
+<rect x="0" y="0" width="32" height="32" transform="translate(4 15)" fill="${useToColor(account, 0.8)}"></rect>
+<rect x="0" y="0" width="32" height="32" transform="translate(-16 20)" fill="${useToColor(account, 0.9)}"></rect>
+</svg>`;
+};
