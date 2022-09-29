419765eb813dc2072f42d71de59ebae78a72d77e
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-09-29 22:33:39 +0800

    clean up main import

--- lib/main.ts
+++ lib/main.ts
@@ -1,40 +1,4 @@
-export interface UseFetchReturn<T> {
-    data: T | null;
-    error: Error | null;
-    time: number;
-}
-
-export interface UseFetchArgs {
-    method?: 'POST' | 'GET' | 'PUT' | 'DELETE';
-    headers?: Headers;
-    baseUrl?: string;
-    body?: Record<string, unknown>;
-    params?: Record<string, string>;
-    bodyToBytes?: boolean;
-}
-
-export const useFetch = async <T>(url: string, args?: UseFetchArgs): Promise<UseFetchReturn<T>> => {
-    const options: RequestInit = {
-        method: args?.method || 'GET',
-        headers: args?.headers || {},
-        body: args?.body ? JSON.stringify(args.body) : null,
-    };
-
-    if (args?.baseUrl) url = args.baseUrl + url;
-    if (args?.params) url += `?${new URLSearchParams(args.params).toString()}`;
-
-    try {
-        const t1 = performance.now();
-        const response = await fetch(url, { ...options });
-        const data = await response.json();
-        const t2 = performance.now();
-
-        return { data, error: null, time: t2 - t1 };
-    } catch (error) {
-        return {
-            data: null,
-            error,
-            time: 0,
-        };
-    }
-};
+export * from './methodes/useAvatar';
+export * from './methodes/useColer';
+export * from './methodes/useCounter';
+export * from './methodes/useFetch';
