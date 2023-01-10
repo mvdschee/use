18fc598ead3dd0598756f589dde1775b249ba130
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-01-10 17:13:06 +0800

    tweaking color values

--- src/methodes/style.ts
+++ src/methodes/style.ts
@@ -21,12 +21,18 @@ interface useColorArgs {
  * Util to transform a string to a color
  * Useful for account based personalisations
  */
-export const useColor = (str: string | null, args: useColorArgs): string => {
+export const useColor = (str: string | null, args?: useColorArgs): string => {
     const source = str || '0';
 
     const stringUniqueHash = [...source].reduce((acc, char) => {
         return char.charCodeAt(0) + ((acc << 5) - acc);
     }, 0);
 
-    return `hsla(${(stringUniqueHash % 360) / (args?.offset || 1)}, 70%, 50%, ${(args?.alpha || 1) * 100}%)`;
+    const hash = stringUniqueHash % 360;
+    let offset = args?.offset || 1;
+
+    if (hash < 50) offset -= 0.4;
+    else if (hash < 130) offset += 0.1;
+
+    return `hsla(${+(hash / offset).toFixed(0)}, 70%, 50%, ${(args?.alpha || 1) * 100}%)`;
 };
