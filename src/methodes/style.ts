b31400b161cebe6a8d7d1be744ac05a5a214b6e0
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-01-28 16:53:19 +0800

    have a rgba output

--- src/methodes/style.ts
+++ src/methodes/style.ts
@@ -15,6 +15,12 @@ interface useColorArgs {
      * @default 1
      */
     alpha?: number;
+
+    /**
+     * output color format as rgba
+     * @default false
+     */
+    rgba?: boolean;
 }
 
 /*
@@ -34,5 +40,30 @@ export const useColor = (str: string | null, args?: useColorArgs): string => {
     if (hash < 50) offset -= 0.4;
     else if (hash < 130) offset += 0.1;
 
-    return `hsla(${+(hash / offset).toFixed(0)}, 70%, 50%, ${(args?.alpha || 1) * 100}%)`;
+    const color = `hsla(${+(hash / offset).toFixed(0)}, 70%, 50%, ${(args?.alpha || 1) * 100}%)`;
+
+    return args?.rgba ? hslaToRgba(color) : color;
+};
+
+// convert hsla to rgba
+const hslaToRgba = (hsla: string): string => {
+    let r: number, g: number, b: number, a: number;
+    let h: number, s: number, l: number;
+    let m: number, c: number, x: number;
+
+    hsla = hsla.replace(/hsla?\(|\)/g, '').replace(/%| /g, '');
+    [h, s, l, a] = hsla.split(',').map((v) => +v);
+
+    c = (1 - Math.abs(2 * l - 1)) * s;
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+    m = l - c / 2;
+
+    if (h < 60) [r, g, b] = [c, x, 0];
+    else if (h < 120) [r, g, b] = [x, c, 0];
+    else if (h < 180) [r, g, b] = [0, c, x];
+    else if (h < 240) [r, g, b] = [0, x, c];
+    else if (h < 300) [r, g, b] = [x, 0, c];
+    else [r, g, b] = [c, 0, x];
+
+    return `rgba(${Math.round((r + m) * 255)}, ${Math.round((g + m) * 255)}, ${Math.round((b + m) * 255)}, ${a})`;
 };
