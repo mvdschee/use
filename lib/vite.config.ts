1f6eea51dfca9dcf0b4b47fc9803d87cc800ac35
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-04-16 09:52:47 +0800

    rework structure to use pnpm and vite

---
+++ lib/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [dts()],
+    build: {
+        lib: {
+            entry: ['src/main.ts'],
+            formats: ['es', 'cjs'],
+            fileName: (format, entryName) => `${entryName}.${format}.js`,
+        },
+    },
+});
