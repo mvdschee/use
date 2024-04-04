5c23cc75f5fd6897471a5b35a158db47c537d159
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2024-04-04 11:24:51 +0700

    fix issue with -1 precision

--- lib/src/methodes/format.ts
+++ lib/src/methodes/format.ts
@@ -6,11 +6,14 @@
 
 export const useTokenDisplay = (value: string | number, precision = 8, enforce = false): string => {
     const number = typeof value === 'number' ? value : parseFloat(value);
-
+    
     if (Number.isNaN(number)) return '0';
 
+    const precisionUnderFlow = precision < 0;
+
     return number.toLocaleString('EN', {
-        maximumFractionDigits: precision,
+        maximumFractionDigits: precisionUnderFlow ? 1 : precision,
         minimumFractionDigits: enforce ? precision : 0,
     });
+
 };
